e0b909c040185749e556ae243af7999d443c6595
Dmitri <dmitriy.chernysh@gmail.com>
2025-01-04 22:35:42 +0200

    + new module for main activity

--- app/src/main/kotlin/com/mobiledevpro/app/MainActivity.kt
+++ app/src/main/kotlin/com/mobiledevpro/app/MainActivity.kt
@@ -3,13 +3,10 @@ package com.mobiledevpro.app
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.runtime.getValue
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.core.view.WindowCompat
-import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.mobiledevpro.app.ui.MainApp
 import com.mobiledevpro.ui.theme.AppTheme
-import com.mobiledevpro.ui.theme.darkModeState
 import org.koin.compose.KoinContext
 
 class MainActivity : ComponentActivity() {
@@ -22,9 +19,9 @@ class MainActivity : ComponentActivity() {
         WindowCompat.setDecorFitsSystemWindows(window, false)
 
         setContent {
-            val darkModeState by darkModeState.collectAsStateWithLifecycle()
+            // val darkModeState by darkModeState.collectAsStateWithLifecycle()
 
-            AppTheme(darkTheme = darkModeState) {
+            AppTheme(darkTheme = true) {
                 KoinContext {
                     MainApp()
                 }
