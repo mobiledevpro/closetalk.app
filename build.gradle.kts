95dfd39ad48e904dcf2794d742bce8510def574e
Dmitri <dmitriy.chernysh@gmail.com>
2025-08-01 23:58:09 +0300

    Updated config

--- build.gradle.kts
+++ build.gradle.kts
@@ -43,7 +43,7 @@ moduleGraphConfig {
     orientation.set(Orientation.LEFT_TO_RIGHT) //optional
     setStyleByModuleType.set(false)
 
-    focusedModulesRegex.set(".*(navigation|home).*")
+    focusedModulesRegex.set(".*(navigation|home|people).*")
 
     theme.set(
         Theme.BASE(
